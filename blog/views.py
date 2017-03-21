b3aa7e17c6b516409340c662a04779ea02b5ab6a
ryutoyasugi <ry86tr2662@gmail.com>
2017-03-21 23:43:14 +0900

    using django-rest-framework

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import User, Entry
+from .serializer import UserSerializer, EntrySerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class EntryViewSet(viewsets.ModelViewSet):
+    queryset = Entry.objects.all()
+    serializer_class = EntrySerializer
